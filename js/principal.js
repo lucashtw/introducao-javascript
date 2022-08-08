b96001da34086ac0ffe0685a19324b377645ae43
lucas <lucashtw@gmail.com>
2022-08-08 20:43:17 -0300

    Adicionando formulario, e enviando dados para tabela

--- js/principal.js
+++ js/principal.js
@@ -40,6 +40,41 @@ for(var i = 0; i< pacientes.length; i++){
 }
 
 
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+
+botaoAdicionar.addEventListener("click",function(event){
+    event.preventDefault();
+    var form = document.querySelector("#form-adiciona");
+
+    var nome = form.nome.value;
+    var peso = form.peso.value;
+    var altura = form.altura.value;
+    var gordura = form.gordura.value;
+
+    var pacienteTR = document.createElement("tr");
+    
+    var nomeTd = document.createElement("td");
+    var pesoTd = document.createElement("td");
+    var alturaTd = document.createElement("td");
+    var gorduraTd = document.createElement("td");
+    var imcTd = document.createElement("td"); 
+
+    nomeTd.textContent = nome;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+    
+    pacienteTR.appendChild(nomeTd);
+    pacienteTR.appendChild(pesoTd);
+    pacienteTR.appendChild(alturaTd);
+    pacienteTR.appendChild(gorduraTd);
+    
+    var tabela = document.querySelector("#tabela-pacientes");
+
+    tabela.appendChild(pacienteTR);
+
+
+});
 
 
 
