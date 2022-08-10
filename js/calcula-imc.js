968c61d57edf58f44cd1ea9ab762ee9e1ad51ac7
lucas <lucashtw@gmail.com>
2022-08-09 21:13:56 -0300

    refatorando o codigo com funções

---
+++ js/calcula-imc.js
@@ -0,0 +1,61 @@
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Aparecida Nutricionista";
+
+var pacientes = document.querySelectorAll(".paciente");
+
+
+
+for(var i = 0; i< pacientes.length; i++){
+    
+    var paciente = pacientes[i];
+
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
+    
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
+    
+    var tdImc = paciente.querySelector(".info-imc");
+    
+    var pesoEhValido = true;
+    var alturaEhValida = true;
+    
+    if(peso <= 0 || peso >= 1000){
+        console.log("Peso inválido");
+        pesoEhValido = false;
+        tdImc.textContent = "Peso Inválido";
+        paciente.classList.add("paciente-invalido");
+    }
+    
+    if(altura <= 0 || altura >= 3.00){
+        console.log("Altura inválida");
+        alturaEhValida = false;
+        tdImc.textContent = "Altura inválida";
+        paciente.classList.add("paciente-invalido");
+    }
+    if(alturaEhValida && pesoEhValido){
+        var imc = calculaImc(peso,altura);
+        tdImc.textContent = imc;
+    }
+}
+
+
+function calculaImc(peso,altura){
+    var imc = 0
+    imc = peso / (altura*altura);
+
+    return imc.toFixed(2);
+}
+
+
+
+
+
+
+
+
+
+
+//
+
+
