5ecdd2c16f8e0f8c8e66621f91ef12866d384a22
lucas <lucashtw@gmail.com>
2022-09-10 22:46:28 -0300

    Tratando a resposta da requisição

--- js/buscar-pacientes.js
+++ js/buscar-pacientes.js
@@ -9,7 +9,14 @@ botaoAdicionar.addEventListener("click",function(){
     xhr.open("GET","https://api-pacientes.herokuapp.com/pacientes");
 
     xhr.addEventListener("load",function(){
-        console.log(xhr.responseText);
+        var resposta = xhr.responseText;
+        var pacientes = JSON.parse(resposta);
+        
+        pacientes.forEach(function(paciente) {
+            adicionaPacienteNaTabela(paciente);
+            
+        });
+
     });
 
     xhr.send();
--- js/form.js
+++ js/form.js
@@ -3,28 +3,27 @@ var botaoAdicionar = document.querySelector("#adicionar-paciente");
 botaoAdicionar.addEventListener("click",function(event){
     event.preventDefault();
     var form = document.querySelector("#form-adiciona");
-    //Extraindo informacoes do paciente do form
     var paciente = obtemPacienteDoFormulario(form);
-    //cria a tr a td do paciente
-    var pacienteTr = montaTr(paciente);    
     
     var erros = validaPaciente(paciente);
-   
-    
-
+    console.log(erros);
     if(erros.length>0){
        exibeMensagensDeErro(erros);
         return;
     }
 
-    var tabela = document.querySelector("#tabela-pacientes");
-
-    tabela.appendChild(pacienteTr);
+    adicionaPacienteNaTabela(paciente);
 
     form.reset();
     var mensagensErro = document.querySelector("#mensagens-erro");
     mensagensErro.innerHTML = "";
 });
+
+function adicionaPacienteNaTabela(paciente){
+    var pacienteTr = montaTr(paciente);
+    var tabela = document.querySelector("#tabela-pacientes");
+    tabela.appendChild(pacienteTr);
+}
 
 function exibeMensagensDeErro(erros){
     var ul = docucument.querySelector("mensagens-erro");
